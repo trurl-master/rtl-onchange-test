0276871688c520a2897df5ff041466f7db50062f
Ivan Galyatin <trurl-master@ya.ru>
2019-10-30 20:32:14 +0100

    Add one more test

--- src/CheckboxWithLabel.test.js
+++ src/CheckboxWithLabel.test.js
@@ -5,10 +5,19 @@ import CheckboxWithLabel from "./CheckboxWithLabel";
 const CHECKBOX = "checkbox";
 const LABEL = "label text";
 
-it("checks!", () => {
+it("works when clicked on the text", () => {
   const { getByText, getByTestId } = render(<CheckboxWithLabel />);
 
   fireEvent.click(getByText(LABEL));
+
+  // fails
+  expect(getByTestId(CHECKBOX).querySelector("input").checked).toBe(true);
+});
+
+it("works when clicked on the label", () => {
+  const { getByText, getByTestId } = render(<CheckboxWithLabel />);
+
+  fireEvent.click(getByTestId(CHECKBOX));
 
   expect(getByTestId(CHECKBOX).querySelector("input").checked).toBe(true);
 });
